e28bfb60771fa5ee48d21d81cd3c54b2d1e9c5a2
creator <wangmingzhang@outlook.com>
2018-11-10 12:28:22 +0800

    not finished yet

--- globals.py
+++ globals.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 
 SEED = 1213
 
-NUM_CLASSES = 250
+NUM_CLASSES = 40
 
 NUM_VIEWS = 12
 
@@ -14,7 +14,12 @@ IMAGE_SHAPE = (227, 227, 3)
 
 VIEWS_IMAGE_SHAPE = (NUM_VIEWS, 227, 227, 3)
 
-TRAIN_LIST = './data/view/train_lists.txt'
+IMAGE_DEPTH = 255
 
-VAL_LIST = './data/view/val_lists.txt'
+TRAIN_LIST = './data/train_lists.txt'
 
+VAL_LIST = './data/val_lists.txt'
+
+TRAIN_EPOCH_NUM = 10
+
+TRAIN_BATCH_SIZE = 32
