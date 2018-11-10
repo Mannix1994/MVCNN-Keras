11bff9e8ce30a5c2d720068cde1ad773ea60b218
creator <wangmingzhang@outlook.com>
2018-11-10 16:20:32 +0800

    finish load data using multi-thread

--- inputs.py
+++ inputs.py
@@ -5,12 +5,19 @@ from __future__ import division
 from __future__ import print_function
 
 import random
+
+import cv2
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
 import globals as _g
-import cv2
+
+
+def load_list(list_file):
+    image_list = np.loadtxt(list_file, dtype=str, skiprows=2)
+    assert len(image_list) == _g.NUM_VIEWS
+    return tf.constant(image_list)
 
 
 def prepare_dataset(name=''):
@@ -25,25 +32,32 @@ def prepare_dataset(name=''):
     one_shot_labels = keras.utils.to_categorical(labels, _g.NUM_CLASSES)
     one_shot_labels.astype(dtype=np.int32)
 
-    def generator():
-        for list_file, label in zip(list_files, one_shot_labels):
-            image_list = np.loadtxt(list_file, dtype=str, skiprows=2)
-            assert len(image_list) == _g.NUM_VIEWS
-            images = []
-            for index, image in enumerate(image_list):
-                image = cv2.imread(image, cv2.IMREAD_UNCHANGED)
-                image = cv2.resize(image, _g.IMAGE_SHAPE[0:2])
-                image = (image-_g.IMAGE_DEPTH/2)/_g.IMAGE_DEPTH
-                image = image[np.newaxis, :]
-                images.append(image)
-            view = np.concatenate(images, axis=0)
-            yield view, label
-
-    dataset = tf.data.Dataset.from_generator(generator, output_types=(tf.float32, tf.int32))
+    dataset = tf.data.Dataset.from_tensor_slices((tf.constant(list_files), tf.constant(one_shot_labels)))
+    dataset = dataset.map(parse_image, num_parallel_calls=16)
     dataset = dataset.batch(_g.TRAIN_BATCH_SIZE)
-    # dataset.map(parse, num_parallel_calls=16)
     dataset = dataset.repeat()
     return dataset
+
+
+def _read_py_function(filename, label):
+    image_lists = np.loadtxt(filename.decode(), dtype=str, skiprows=2)
+    assert len(image_lists) == _g.NUM_VIEWS
+    images = [cv2.imread(image_name, cv2.IMREAD_UNCHANGED).astype(np.float32) for image_name in image_lists]
+    # resize image
+    resized_images = [cv2.resize(image, _g.IMAGE_SHAPE[0:2]) for image in images]
+    # scale_image
+    scaled_images = [(image - _g.IMAGE_DEPTH // 2) / _g.IMAGE_DEPTH for image in resized_images]
+    # add axis
+    axis_images = [image[np.newaxis, :] for image in scaled_images]
+    # concat images
+    image_decoded = np.concatenate(axis_images, axis=0)
+    # convert type
+    image_decoded.astype(np.float32)
+    return image_decoded.astype(np.float32), label
+
+
+def parse_image(image_file, label):
+    return tf.py_func(_read_py_function, [image_file, label], [tf.float32, label.dtype])
 
 
 def test_inputs():
@@ -56,10 +70,9 @@ def test_inputs():
 
     with tf.Session() as sess:
         for i in range(10):
-            data = sess.run(next_data)
-            print(len(data), data[0].shape, len(data[1]), np.min(data[0]), np.max(data[0]))
+            data, label = sess.run(next_data)
+            print(len(data), len(label), data.shape, np.min(data), np.max(data))
 
 
 if __name__ == '__main__':
     test_inputs()
-
--- train.py
+++ train.py
@@ -5,19 +5,35 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow import keras
+import tensorflow as tf
 
 import globals as _g
 import model
 import inputs
 
 
-if __name__ == '__main__':
-    train_dataset = inputs.prepare_dataset(_g.TRAIN_LIST)
-    val_dataset = inputs.prepare_dataset(_g.VAL_LIST)
-    model = model.inference_multi_view()
-    model.compile(optimizer=keras.optimizers.Adam(lr=0.001, decay=1e-4),
-                  loss=keras.losses.categorical_crossentropy,
-                  metrics=[keras.metrics.categorical_accuracy])
-    model.fit(train_dataset, epochs=1, steps_per_epoch=100, validation_data=val_dataset, validation_steps=32)
-    # test_pre_process()
+def input_fn():
+    return inputs.prepare_dataset(_g.TRAIN_LIST)
 
+
+if __name__ == '__main__':
+
+    train_with_multi_gpu = True
+
+    model = model.inference_multi_view()
+
+    if not train_with_multi_gpu:
+        train_dataset = inputs.prepare_dataset(_g.TRAIN_LIST)
+        val_dataset = inputs.prepare_dataset(_g.VAL_LIST)
+
+        model.compile(optimizer=keras.optimizers.Adam(lr=0.001, decay=1e-4),
+                      loss=keras.losses.categorical_crossentropy,
+                      metrics=[keras.metrics.categorical_accuracy])
+
+        model.fit(train_dataset, epochs=10, steps_per_epoch=100, validation_data=val_dataset, validation_steps=32)
+    else:
+        model.compile(optimizer=tf.train.AdamOptimizer())
+        strategy = tf.contrib.distribute.MirroredStrategy()
+        config = tf.estimator.RunConfig(train_distribute=strategy)
+        keras_estimator = tf.keras.estimator.model_to_estimator(keras_model=model, config=config, model_dir='./model_dir')
+        keras_estimator.train(input_fn=input_fn, steps=10)
